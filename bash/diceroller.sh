bd23f0d6ad609b2548c2cfb7112779e1bc57ed22
tojotomy <200425013@student.georgianc.on.ca>
2020-06-18 08:19:47 -0700

    Lab2

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,7 +6,6 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
-# roll the dice and display the result
 echo "
 Rolling...
 $(( RANDOM % 6 + 1)) rolled
@@ -19,9 +18,4 @@ $(( RANDOM % 6 + 1))-T
 $(( RANDOM % 6 + 1))-O
 $(( RANDOM % 6 + 1))-J
 $(( RANDOM % 6 + 1))-O
-"
-echo "rolling one 20 sided die"
-echo "
-rolling......
-$((RANDOM % 20+ 1))  Rolled
 "
